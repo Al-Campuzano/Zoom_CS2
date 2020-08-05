90301bbe538f15059e5d56f058c4c7d36b751b49
Al-Campuzano <al.campuzano@shopify.com>
2020-08-05 18:45:58 -0400

    whole shape using two loops

--- Shape_Of_Things/main.cpp
+++ Shape_Of_Things/main.cpp
@@ -9,13 +9,25 @@ void printChar(char c, int n) {
 }
 
 int main(){
+  int height = 5;
 
-  for (int i = 0; i < 5; i++) {
+// This prints the top half of the shape
+  for (int i = 0; i < height; i++) {
     printChar(' ', i);
     printChar('#', i+1);
-    printChar(' ', 9-(i*2+1));
-    printChar(' ', 9-(i*2+1));
+    printChar(' ', 8-(i*2));
+    printChar(' ', 8-(i*2));
     printChar('#', i+1);
+    cout << '\n';
+  }
+
+// This prints the bottom half of the shape
+  for (int i = 0; i < height; i++) {
+    printChar(' ', height - (i+1));
+    printChar('#', height - i);
+    printChar(' ', i * 2);
+    printChar(' ', i * 2);
+    printChar('#', height - i);
     cout << '\n';
   }
 
