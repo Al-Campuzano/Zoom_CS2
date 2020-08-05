ff84928d27f592c53890a4b102eb6d3014972d16
Al-Campuzano <al.campuzano@shopify.com>
2020-08-05 16:18:57 -0400

    second iteration, half of the shape

--- Shape_Of_Things/main.cpp
+++ Shape_Of_Things/main.cpp
@@ -13,6 +13,9 @@ int main(){
   for (int i = 0; i < 5; i++) {
     printChar(' ', i);
     printChar('#', i+1);
+    printChar(' ', 9-(i*2+1));
+    printChar(' ', 9-(i*2+1));
+    printChar('#', i+1);
     cout << '\n';
   }
 
