65d9f176b2df7812b858971352fc60a1f760c06e
Al-Campuzano <al.campuzano@shopify.com>
2020-08-05 16:11:50 -0400

    first iteration, quarter of the shape

--- Shape_Of_Things/main.cpp
+++ Shape_Of_Things/main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+void printChar(char c, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << c;
+  }
+}
+
 int main(){
+
+  for (int i = 0; i < 5; i++) {
+    printChar(' ', i);
+    printChar('#', i+1);
+    cout << '\n';
+  }
 
   return 0;
 }
