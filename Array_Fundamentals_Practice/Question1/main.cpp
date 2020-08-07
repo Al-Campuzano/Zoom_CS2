17eeb9ce2a156f1d012f7ede8083fc143353edc1
Al-Campuzano <al.campuzano@shopify.com>
2020-08-07 09:54:16 -0400

    Add Question 2 program

--- Array_Fundamentals_Practice/Question1/main.cpp
+++ Array_Fundamentals_Practice/Question1/main.cpp
@@ -17,13 +17,15 @@ bool isSorted(int array[], int numItems) {
 int main() {
   const int TEST_ARRAY_1_SIZE = 7;
   const int TEST_ARRAY_2_SIZE = 1;
+
   int testArray1[TEST_ARRAY_1_SIZE] = {4, 2, 4, 5, 6, 7, 8}; // false
   int testArray2[TEST_ARRAY_1_SIZE] = {1, 2, 4, 5, 6, 7, 8}; // true
   int testArray3[TEST_ARRAY_1_SIZE] = {1, 2, 4, 5, 6, 7, 3}; // false
   int testArray4[TEST_ARRAY_1_SIZE] = {2, 2, 4, 5, 6, 7, 10}; // true
   int testArray5[TEST_ARRAY_1_SIZE] = {8, 7, 6, 5, 4, 3, 2}; // false
   int testArray6[TEST_ARRAY_2_SIZE] = {3}; // true
-  int testArray7[TEST_ARRAY_1_SIZE] = {}; // true
+  int testArray7[TEST_ARRAY_1_SIZE]; // result might be unexpected since array is not initialized
+
   cout << "For test 1 isSorted returned " << isSorted(testArray1, TEST_ARRAY_1_SIZE) << endl;
   cout << "For test 2 isSorted returned " << isSorted(testArray2, TEST_ARRAY_1_SIZE) << endl;
   cout << "For test 3 isSorted returned " << isSorted(testArray3, TEST_ARRAY_1_SIZE) << endl;
@@ -31,5 +33,6 @@ int main() {
   cout << "For test 5 isSorted returned " << isSorted(testArray5, TEST_ARRAY_1_SIZE) << endl;
   cout << "For test 6 isSorted returned " << isSorted(testArray6, TEST_ARRAY_2_SIZE) << endl;
   cout << "For test 7 isSorted returned " << isSorted(testArray7, TEST_ARRAY_1_SIZE) << endl;
+
   return 0;
 }
--- Array_Fundamentals_Practice/Question2/main.cpp
+++ Array_Fundamentals_Practice/Question2/main.cpp
@@ -1,8 +1,35 @@
+// Write a C++ program that defines a constant (fixed) array of at least 10 characters.
+// This must be an array of char, not a string type.
+// Loop through the array and count how many characters are uppercase.
+// Demonstrate that your code works correctly in main().
+
 #include <iostream>
 
 using namespace std;
 
+int countUpperChars(const char array[], int numItems) {
+  int upperCount = 0;
+
+  for (int i = 0; i < numItems; i++) {
+    if (array[i] >= 'A' && array[i] <= 'Z'){
+      upperCount++;
+    }
+  }
+
+  return upperCount;
+}
+
 int main() {
+  const int ARRAY_SIZE = 10;
+  const char testArray1[ARRAY_SIZE] = {'a', 'a', 'h', 'e', 'l', 'l', 'o', '!', 'z', '2'};
+  const char testArray2[ARRAY_SIZE] = {};
+  const char testArray3[ARRAY_SIZE] = {'A', 'A', 'H', 'E', 'L', 'L', 'O', 'I', 'Z', 'S'};
+  const char testArray4[ARRAY_SIZE] = {'A', 'a', 'H', 'e', 'l', 'l', 'o', '!', 'Z', '2'};
+
+  cout << "testArray1 has " << countUpperChars(testArray1, ARRAY_SIZE) << " uppercase characters\n";
+  cout << "testArray2 has " << countUpperChars(testArray2, ARRAY_SIZE) << " uppercase characters\n";
+  cout << "testArray3 has " << countUpperChars(testArray3, ARRAY_SIZE) << " uppercase characters\n";
+  cout << "testArray4 has " << countUpperChars(testArray4, ARRAY_SIZE) << " uppercase characters\n";
 
   return 0;
 }
