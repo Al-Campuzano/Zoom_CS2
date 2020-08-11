4fc6127d8fec2ac84c01aa7e84928afca09fc257
Al-Campuzano <al.campuzano@shopify.com>
2020-08-11 10:42:20 -0400

    added loop to fill up  array

--- Dominoes/main.cpp
+++ Dominoes/main.cpp
@@ -10,5 +10,18 @@ struct domino {
 int main() {
   // short int test = 6;
   // cout << " Dominoes " << sizeof(test);
+  const short int MAX_PIPS = 6; // size of highest domino piece
+  const short int TILES_NUM = (MAX_PIPS + 1) * (MAX_PIPS + 2)/2; // a double six set of dominoes has 28 tiles
+  domino set[TILES_NUM];
+
+  int index = 0;
+  for (int a = 0; a < MAX_PIPS; a++) {
+    for (int b = a; b < MAX_PIPS; b++) {
+      set[index].sideA = a;
+      set[index].sideB = b;
+      index++;
+    }
+  }
+
   return 0;
 }
