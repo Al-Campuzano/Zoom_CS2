8fe4246296c3035743526180304a762406a0b0c1
Al-Campuzano <al.campuzano@shopify.com>
2020-08-12 13:10:21 -0400

    fixed issue with extra hash in middle, added comments

--- Shape_Of_Things/main.cpp
+++ Shape_Of_Things/main.cpp
@@ -17,12 +17,16 @@ int main() {
   if (height % 2 == 0) {
     // This prints the top half of the shape
     for (int i = 0; i < height/2; i++) {
+      // left half of shape
       printChar(' ', i);
       printChar('#', i+1);
+      // extra character in the middle
       if (i == height/2 - 1) {
         printChar('#', 1);
+      } else { // otherwise add blanks plus one to compensate for extra character
+        printChar(' ', (height-2)-(i*2) +1);
       }
-      printChar(' ', (height-2)-(i*2));
+      // right half of shape
       printChar(' ', (height-2)-(i*2));
       printChar('#', i+1);
       cout << '\n';
@@ -30,12 +34,16 @@ int main() {
 
     // This prints the bottom half of the shape
     for (int i = height/2; i < height; i++) {
+      // left half of shape
       printChar(' ', height - (i+1));
       printChar('#', height - i);
+      // extra character in the middle
       if (i == height/2) {
         printChar('#', 1);
+      } else { // otherwise add blanks plus one to compensate for extra character
+        printChar(' ', i * 2 - height + 1);
       }
-      printChar(' ', i * 2 - height);
+      // right half of shape
       printChar(' ', i * 2 - height);
       printChar('#', height - i);
       cout << '\n';
