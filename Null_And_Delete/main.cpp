1d9cdc546e59eac1e76b2facf96cf2d7a3266596
Al-Campuzano <al.campuzano@shopify.com>
2020-08-17 08:21:58 -0400

    initializing branch

---
+++ Null_And_Delete/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int * pointerArray[4];
+
+  pointerArray[0] = new int;
+  pointerArray[1] = new int;
+  pointerArray[2] = NULL;
+  pointerArray[3] = new int;
+
+  *(pointerArray[0]) = 1;
+  *(pointerArray[1]) = 10;
+  *(pointerArray[3]) = 50;
+
+  return 0;
+}
