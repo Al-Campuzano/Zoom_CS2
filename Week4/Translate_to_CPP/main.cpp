8022fb0708c34ebc38ace1ad5ab23c5e7117f90a
Al-Campuzano <al.campuzano@shopify.com>
2020-08-20 09:19:16 -0400

    full solution

--- Week4/Translate_to_CPP/main.cpp
+++ Week4/Translate_to_CPP/main.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
-int main() {
+float getAverageScore(int scoreArray[], int length) {
+  float total = 0.0;
 
+  for (size_t i = 0; i < length; i++) {
+    total += scoreArray[i];
+  }
+
+  return total / length;
+}
+
+int main() {
+  const int ARRAY_SIZE = 10;
+  // initializing all elements to zero to avoid having any garbage values
+  int scores[ARRAY_SIZE] = {0};
+
+  for (size_t i = 0; i < ARRAY_SIZE; i+=2) {
+    scores[i] = i;
+  }
+
+  cout << "Average score: " << getAverageScore(scores, ARRAY_SIZE);
   return 0;
 }
