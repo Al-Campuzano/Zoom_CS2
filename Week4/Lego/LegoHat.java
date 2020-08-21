46feb4aa2d12d041523de2eba8140f51e247eb2f
Al-Campuzano <al.campuzano@shopify.com>
2020-08-21 08:45:13 -0400

    added hat toString method

--- Week4/Lego/LegoHat.java
+++ Week4/Lego/LegoHat.java
@@ -1,4 +1,25 @@
 public class LegoHat {
+  String style;
+  int size; // should be between 1-10
 
-  
+  public LegoHat(String newStyle, int newSize) {
+    style = newStyle;
+    size = newSize;
+  }
+
+  public String toString() {
+    String sizeInWords = "small";
+    if (size > 3 && size <= 6) {
+      sizeInWords = "medium";
+    } else if (size > 6) {
+      sizeInWords = "large";
+    }
+
+    return "a " + sizeInWords + " " + style;
+  }
+
+  public int computeStyle(String season) {
+    return 0;
+  }
+
 }
--- Week4/Lego/TestClass.java
+++ Week4/Lego/TestClass.java
@@ -1,8 +1,10 @@
 public class TestClass {
 
   public static void main(String[] args) {
+    System.out.println("Testing hat");
+    LegoHat hat = new LegoHat("toque", 5);
 
-
+    System.out.println(hat.toString());
 
   }
 
