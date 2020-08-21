76533cf9fc964f806e3f8c79c5f2d9d3a0c293f2
Al-Campuzano <al.campuzano@shopify.com>
2020-08-21 09:36:25 -0400

    added computeStyle hat method

--- Week4/Lego/LegoHat.java
+++ Week4/Lego/LegoHat.java
@@ -1,5 +1,5 @@
 public class LegoHat {
-  String style;
+  String style; // options will be toque, sun visor, sombrero, helmet, bowler, fedora
   int size; // should be between 1-10
 
   public LegoHat(String newStyle, int newSize) {
@@ -8,18 +8,60 @@ public class LegoHat {
   }
 
   public String toString() {
-    String sizeInWords = "small";
-    if (size > 3 && size <= 6) {
-      sizeInWords = "medium";
+    String sizeInWords = "medium";
+    if (size <= 3) {
+      sizeInWords = "small";
     } else if (size > 6) {
       sizeInWords = "large";
     }
-
     return "a " + sizeInWords + " " + style;
   }
 
+  // Sombreros are ALWAYS stylish so they always get a 10.
+  // Fedoras are NEVER stylish so they always get a 1.
+  // Helmets are practical so they always get a 5.
+  // Toques are stylish in winter and somewhat stylish in the fall and spring,
+  // but not in summer.
+  // Sun visors are stylish in summer, somewhat in fall and spring, not in winter.
+  // Bowler hats are most stylish in fall and spring, less so in summer and winter.
+  // seasons should use 'fall' not 'autumn'
   public int computeStyle(String season) {
-    return 0;
+    switch(style) {
+      case "sombrero":
+        return 10;
+      case "fedora":
+        return 1;
+      case "helmet":
+        return 5;
+      case "toque":
+        switch(season) {
+          case "winter":
+            return 9;
+          case "summer":
+            return 1;
+          default:
+            return 6;
+        }
+      case "sun visor":
+        switch(season) {
+          case "winter":
+            return 1;
+          case "summer":
+            return 9;
+          default:
+            return 4;
+        }
+      case "bowler":
+        switch(season) {
+          case "fall":
+          case "spring":
+            return 7;
+          default:
+            return 3;
+        }
+      default:
+        return 5;
+    }
   }
 
 }
--- Week4/Lego/TestClass.java
+++ Week4/Lego/TestClass.java
@@ -1,11 +1,11 @@
 public class TestClass {
 
   public static void main(String[] args) {
-    System.out.println("Testing hat");
-    LegoHat hat = new LegoHat("toque", 5);
+    System.out.println("Testing computeStyle");
+    LegoHat hat = new LegoHat("bowler", 5);
 
     System.out.println(hat.toString());
-
+    System.out.println(hat.computeStyle("fall"));
   }
 
 }
