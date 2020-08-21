91bb63cd08c5619c64438683e18445c298a7c6b2
Al-Campuzano <al.campuzano@shopify.com>
2020-08-21 10:38:35 -0400

    swap, wear, and place methods

--- Week4/Lego/LegoMinifigure.java
+++ Week4/Lego/LegoMinifigure.java
@@ -10,4 +10,31 @@ public class LegoMinifigure {
     leftHand = left;
     rightHand = right;
   }
+
+  public String toString() {
+    return "TODO";
+  }
+
+  public void swapHands() {
+    LegoItem temp = leftHand;
+    leftHand = rightHand;
+    rightHand = temp;
+  }
+
+  public void wearHat(LegoHat hat) {
+    this.hat = hat;
+  }
+
+  public void placeInLeftHand(LegoItem item) {
+    leftHand = item;
+  }
+
+  public void placeInRightHand(LegoItem item) {
+    rightHand = item;
+  }
+
+  public boolean isGood(String season, float threshold) {
+
+    return true;
+  }
 }
--- Week4/Lego/TestClass.java
+++ Week4/Lego/TestClass.java
@@ -3,23 +3,33 @@ public class TestClass {
   public static void main(String[] args) {
     // System.out.println("Testing computeStyle");
     LegoHat hat = new LegoHat("bowler", 8);
+    LegoHat sombrero = new LegoHat("sombrero", 1);
     //
     // System.out.println(hat.toString());
     // System.out.println(hat.computeStyle("fall"));
 
     // System.out.println("Testing LegoItem");
     LegoItem item = new LegoItem("sword", 8.8f);
+    LegoItem item2 = new LegoItem("shield", 15.0f);
     //
     // System.out.println(item.toString());
     // System.out.println(item.isHeavy(10.1f));
 
     System.out.println("Testing LegoMinifigure");
-    LegoMinifigure figure = new LegoMinifigure("Al", hat, item, item);
+    LegoMinifigure figure = new LegoMinifigure("Al", hat, item, item2);
 
     System.out.println(figure.name);
     System.out.println(figure.hat);
-    System.out.println(figure.rightHand);
     System.out.println(figure.leftHand);
+    System.out.println(figure.rightHand);
+    figure.swapHands();
+    figure.wearHat(sombrero);
+    figure.placeInLeftHand(null);
+    figure.placeInRightHand(item2);
+    System.out.println(figure.name);
+    System.out.println(figure.hat);
+    System.out.println(figure.leftHand);
+    System.out.println(figure.rightHand);
   }
 
 }
