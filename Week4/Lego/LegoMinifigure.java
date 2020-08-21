f217721393d5b93c2966044139612180ba31b204
Al-Campuzano <al.campuzano@shopify.com>
2020-08-21 09:56:53 -0400

    minifigure instance variables and constructor

--- Week4/Lego/LegoMinifigure.java
+++ Week4/Lego/LegoMinifigure.java
@@ -1,4 +1,13 @@
 public class LegoMinifigure {
+  String name;
+  LegoHat hat;
+  LegoItem leftHand;
+  LegoItem rightHand;
 
-
+  public LegoMinifigure(String newName, LegoHat newHat, LegoItem left, LegoItem right) {
+    name = newName;
+    hat = newHat;
+    leftHand = left;
+    rightHand = right;
+  }
 }
--- Week4/Lego/TestClass.java
+++ Week4/Lego/TestClass.java
@@ -2,16 +2,24 @@ public class TestClass {
 
   public static void main(String[] args) {
     // System.out.println("Testing computeStyle");
-    // LegoHat hat = new LegoHat("bowler", 5);
+    LegoHat hat = new LegoHat("bowler", 8);
     //
     // System.out.println(hat.toString());
     // System.out.println(hat.computeStyle("fall"));
 
-    System.out.println("Testing LegoItem");
+    // System.out.println("Testing LegoItem");
     LegoItem item = new LegoItem("sword", 8.8f);
+    //
+    // System.out.println(item.toString());
+    // System.out.println(item.isHeavy(10.1f));
 
-    System.out.println(item.toString());
-    System.out.println(item.isHeavy(10.1f));
+    System.out.println("Testing LegoMinifigure");
+    LegoMinifigure figure = new LegoMinifigure("Al", hat, item, item);
+
+    System.out.println(figure.name);
+    System.out.println(figure.hat);
+    System.out.println(figure.rightHand);
+    System.out.println(figure.leftHand);
   }
 
 }
