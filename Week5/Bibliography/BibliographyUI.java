f144f7cd6132c1a0a2a1a7a94063fea2d18d8b2a
Al-Campuzano <al.campuzano@shopify.com>
2020-09-03 09:55:35 -0400

    implemented UI for most published author

--- Week5/Bibliography/BibliographyUI.java
+++ Week5/Bibliography/BibliographyUI.java
@@ -15,7 +15,7 @@ public class BibliographyUI {
     // Get the user's choice
     Scanner input = new Scanner(System.in);
     int mainChoice = input.nextInt();
-
+    // continue loop as long as user does not 'exit' by selecting '4'
     while (mainChoice != 4) {
       switch (mainChoice) {
         case 1:
@@ -24,13 +24,16 @@ public class BibliographyUI {
         case 2:
           addArticleOption();
           break;
+        case 3:
+          findMostPublishedOption();
+          break;
         default:
           System.out.println("Not a valid choice, please try again");
       }
       printOptions();
       mainChoice = input.nextInt();
     }
-    
+
     System.out.println("\nBye!");
   }
 
@@ -89,46 +92,15 @@ public class BibliographyUI {
     data.addArticleToIssue(userTitle, userAuthor, issuesAvail.get(issueChoice - 1));
   }
 
+  public static void findMostPublishedOption() {
+    System.out.println("\nThe most published author is " + data.getMostPublished());
+  }
+
   public static void main(String[] args) {
 
     BibliographyUI userInterface = new BibliographyUI(new BibliographyDatabase(Journal.getSampleJournals()));
     userInterface.runProgram();
 
-    // System.out.println("TESTING BibliographyUI");
-    // Article art1 = new Article("This is the Title", "This be the Author", null);
-    // System.out.println(art1.getTitle());
-    // System.out.println(art1.getAuthor());
-    // BibliographyDatabase data = new BibliographyDatabase(Journal.getSampleJournals());
-    // System.out.println(data.get(1).equals(data.get(0)));
-    // System.out.println(data.get(1).getTitle());
-
-
-    // ArrayList<Journal> sJournals = Journal.getSampleJournals();
-
-    //data.addListOfJournals(sJournals);
-    // Journal jour1 = new Journal("Added Journal");
-    // data.addJournal(jour1);
-
-    // ArrayList<Issue> listOfIssues = data.getIssues();
-    // System.out.println("Issues in DB:");
-    // System.out.println(listOfIssues.get(2));
-    // data.addArticleToIssue("Added Article", "Second Author", listOfIssues.get(2));
-    // for (Issue i : listOfIssues) {
-    //   System.out.println(i);
-    // }
-    //
-    // ArrayList<Article> articlesFrom1910 = data.getArticlesFromYear(1910);
-    // System.out.println("Articles from 1910: ");
-    // for (Article a : articlesFrom1910) {
-    //   System.out.println(a);
-    // }
-    //
-    // System.out.println("Most published:");
-    // System.out.println(data.getMostPublished());
-    // System.out.println(data.journals.get(4).getTitle());
-    // System.out.println(data.removeJournal("Added Journal"));
-    // System.out.println(sJournals.indexOf("First Journal"));
-    // System.out.println(sJournals.get(0).getTitle());
   }
 
 }
