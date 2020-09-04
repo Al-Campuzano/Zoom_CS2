c0540163a6a0462a69b3299f4f2e065547c55038
Al-Campuzano <al.campuzano@shopify.com>
2020-09-04 13:20:20 -0400

    initializing branch

---
+++ Week6/Implement-Hierarchy/HardCover.java
@@ -0,0 +1,4 @@
+public class HardCover extends Book {
+
+
+}
---
+++ Week6/Implement-Hierarchy/TestHierarchy.java
@@ -0,0 +1,9 @@
+public class TestHierarchy {
+
+  public static void main(String[] args) {
+
+    System.out.println("Implementing Hierarchy Practice");
+
+  }
+
+}
