a5471ba0e7152250bff0b6750a1f99254c802209
Al-Campuzano <al.campuzano@shopify.com>
2020-09-15 08:48:03 -0400

    implemented Fibonacci solution

--- Week8/Recursion-in-Code/Fibonacci.java
+++ Week8/Recursion-in-Code/Fibonacci.java
@@ -1,9 +1,18 @@
 public class Fibonacci {
 
+  public static int Fib(int n) {
+    if (n == 0 || n == 1) {
+      return n;
+    }
+    return Fib(n-1) + Fib(n-2);
+  }
+
   public static void main(String[] args) {
 
     System.out.println("Fibonacci Sequence");
-
+    for (int i = 0; i < 15; i++) {
+      System.out.print(Fib(i) + ", ");
+    }
   }
 
 }
